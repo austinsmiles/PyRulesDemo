6aeefdd24d69e42de8aeb265791c6d92a4c3aaf9
Auzzy <austinsmiles@gmail.com>
2020-05-23 23:27:33 -0400

    method name change

--- pkg/main/main.py
+++ pkg/main/main.py
@@ -2,7 +2,7 @@ from pkg import *
 import yaml
 
 
-def load_config(config_file):
+def read_yaml(config_file):
     with open(config_file, 'r') as stream:
         try:
             return yaml.safe_load(stream)
@@ -10,11 +10,9 @@ def load_config(config_file):
             print(exc)
 
 
-#rules=load_config('../rules/rules.yaml')
-#rules=load_config('../../rules/validation.yaml')
-rules=load_config('../../rules/mongoRuleSample.yaml')
+# rules=load_config('../rules/rules.yaml')
+# rules=load_config('../../rules/validation.yaml')
+rules = read_yaml('../../rules/mongoRuleSample.yaml')
 
 for rule in rules.values():
     extension_map[rule["extension"]].process(function=rule["operation"], **rule)
-
-
