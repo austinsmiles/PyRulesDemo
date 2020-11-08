ee66a914c135ded4f2cd5a80286a9ef2d1dc2c6a
Austin Perez <austinsmiles@gmail.com>
2020-11-08 18:44:50 -0500

    Added some extensions for queue processing

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -3,14 +3,19 @@ print(f'Invoking __init__.py for {__name__}')
 import pkg.components
 from pkg.common.utils import read_yaml
 
-pkg.components.config = read_yaml('../../config/config.yaml')
+pkg.components.config = read_yaml('config/config.yaml')
 
 import pkg.extension.componentOperations as componentOperation
 import pkg.extension.transportOperations as transportOperation
 import pkg.extension.mongoOperations as mongoOperation
+import pkg.extension.producerConsumerOperations as producerConsumerOperation
+import pkg.extension.pipelineOperations as pipelineOperation
+
 
 extension_map = {
     'componentOperation': componentOperation,
     'transportOperation': transportOperation,
-    'mongoOperation': mongoOperation
+    'mongoOperation': mongoOperation,
+    'pipelineOperation': pipelineOperation,
+    'producerConsumerOperation': producerConsumerOperation
 }
